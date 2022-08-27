55ccf04e88f44a66c0e84179d0205dcbfd6407d7
mauromacb <mauricio.macb@hotmail.com>
2022-08-27 13:16:06 -0500

    Corecciones
    - Catálogos

--- resources/views/catalogo.blade.php
+++ resources/views/catalogo.blade.php
@@ -9,18 +9,18 @@
                             @foreach($productos as $detalle)
                                 <div class="form-group panel panel-default col-sm-3">
                                     <div class="panel-heading">
-                                        <h3 class="panel-title">{{$detalle->nombre}}</h3>
+                                        <p class="panel-title" style="font-size: 13px;">{{$detalle->nombre}}<p>
                                     </div>
                                     <div class="panel-heading">
-                                       <center> <h3 class="panel-title">Precio: {{$detalle->valor}}</h3></center>
+                                       <center> <h3 class="panel-title" >Precio: {{$detalle->valor}}</h3></center>
                                     </div>
                                     <div class="panel-body">
                                         <p class="card-text">{{$detalle->codigo}}</p>
                                         @if(empty($detalle->imagen))
-                                            <img class="img-responsive" src="{{url('vendor/crudbooster/avatar.jpg')}}" height="150">
+                                            <img class="img-responsive" src="{{url('vendor/crudbooster/avatar.jpg')}}" >
                                         @else
-                                            <img class="img-responsive" src="{{url($detalle->imagen)}}">
-                                
+                                            <img class="img-responsive" src="{{url($detalle->imagen)}}" height="150">
+
                                         @endif
                                         <p class="card-text">{{$detalle->stock}} unidades</p>
                                     </div>
